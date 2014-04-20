61112a3a700d070249020b28313608d123779f75
Daniel Eklund <doeklund@gmail.com>
2014-04-20 10:02:02 -0400

    Just putting in some stubbed messages that more or less match the API for Base64 as shown here:    http://www.erlang.org/doc/man/base64.html

---
+++ src/crockford32.erl
@@ -0,0 +1,19 @@
+-module(crockford32).
+
+-export([encode/1,
+         encode_to_string/1,
+         decode/1,
+         decode_to_string/1
+        ]).
+
+encode(Data) ->
+	ok.
+
+decode(Data) ->
+    ok.
+
+encode_to_string(Data)->
+    ok.
+
+decode_to_string(Data) ->
+    ok.
